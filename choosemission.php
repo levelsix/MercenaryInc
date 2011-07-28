a479c6567e626a1283c31da3e58733d5199a4408
Conrad Chan <conradchan@Conrad-Chans-MacBook-Pro.local>
2011-07-28 00:05:11 -0700

    prints out mission info correctly

--- choosemission.php
+++ choosemission.php
@@ -6,26 +6,45 @@
 <body>
 <?php 
 include("topmenu.php");
-include("properties/dbproperties.php");
 mysql_connect($server,$user,$password);
 @mysql_select_db($database) or die( "Unable to select database");
   
-
-$query="SELECT * FROM missions WHERE minlevel <= ". $playerlevel . ";";
+$query="SELECT * FROM missions WHERE min_level <= ". $playerlevel . ";";
 $result=mysql_query($query);
 $num=mysql_numrows($result);
-echo "Name :" . mysql_result($result, 0, "name");
-print "  ";
-echo "Level: " . mysql_result($result,0,"level");
-print "  ";
-echo "Type: " . mysql_result($result,0,"type");    
-print "  ";
-echo "Attack: " . mysql_result($result,0,"attack");    
-    
+
+
+if ($num == 0) { 
+	echo "No missions available";
+} else {
+
+	
+	print "Choose your mission: <br><br>";
+	$i = 0;
+	while ($i < $num) {
+		print "Title: " . mysql_result($result,$i,"name") . "<br>";
+		print "Description: " . mysql_result($result,$i,"description") . "<br>";
+		print "Cost: " . mysql_result($result,$i,"energy_cost") . " energy<br>";
+		print "Will Gain: " . mysql_result($result,$i,"exp_gained") . " exp<br>";
+		print "Will Gain " . mysql_result($result,$i,"min_gold_gained") . " - ";
+		print mysql_result($result, $i, "max_gold_gained") . "<br>";
+		print "Chance of getting loot: " . mysql_result($result,$i,"chance_of_loot") . "<br>";
+		
+		$item_id = mysql_result($result,$i,"item_id");
+		$query="SELECT * FROM items WHERE id = ". $item_id . ";";
+		$itemresult=mysql_query($query);
+		print "You're not supposed to know this but the item you might get is the ";
+		print mysql_result($itemresult, 0, "name");
+		
+		print "<br><br>";
+		
+		$i++;
+	}
+	
+}
+
+
 mysql_close(); 
-
-
-Choose your mission:
 
 ?>
 </body>
