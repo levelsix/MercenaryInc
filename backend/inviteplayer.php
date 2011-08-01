3094aa0a7a47e442d337899ec73f5a3fa56c093e
Calvin Tuong <calvin.tuong@gmail.com>
2011-07-31 19:09:47 -0700

    submitting agency invitations now work

--- backend/inviteplayer.php
+++ backend/inviteplayer.php
@@ -1,5 +1,22 @@
 <?php
-$agencyCode = $_SESSION["agencyCode"];
+include("../properties/dbproperties.php");
+
+mysql_connect($server, $user, $password);
+@mysql_select_db($database) or die("Unable to select database");
+session_start();
+
+$agencyCode = $_GET['agencyCode'];
+$userId = $_SESSION['userID'];
+
+$userQuery = "SELECT * FROM users WHERE agency_code = " . $agencyCode . ";";
+$userResult = mysql_query($userQuery);
+$inviteeId = mysql_result($userResult, 0, "id");
+
+$insertInvitation = "INSERT IGNORE INTO agencies (user_one_id, user_two_id, accepted) VALUES "
+. "(" . $userId . ", " . $inviteeId . ", 0);";
+
+mysql_query($insertInvitation) or die(mysql_error());
+mysql_close();
 
 header("Location: ../recruit.php");
 exit;
