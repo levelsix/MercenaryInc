0722c1514fc99682bac7605de5ae7bbd6e23d4d8
Calvin Tuong <calvin.tuong@gmail.com>
2011-08-03 00:11:11 -0700

    added last login update statement to login.php

--- backend/login.php
+++ backend/login.php
@@ -1,7 +1,18 @@
-<?php 
+<?php
+include("../properties/dbproperties.php"); 
+
+// Set the ID in the session
 $id = $_POST['id'];
 session_start();
 $_SESSION['userID'] = $id;
+
+// Update last_login in database
+mysql_connect($server, $user, $password);
+@mysql_select_db($database) or die("Unable to select database");
+
+$timestampUpdate = "UPDATE users SET last_login = CURRENT_TIMESTAMP WHERE id = " . $id . ";";
+mysql_query($timestampUpdate) or die(mysql_error());
+
 header("Location: ../charhome.php");
 exit;
 ?>
