2b6f8db5efd31cb158fee3f53bf67b7159ace1a8
Calvin Tuong <calvin.tuong@gmail.com>
2011-08-16 00:16:55 -0700

    fixed <form> syntax error

--- addplayertobounty.php
+++ addplayertobounty.php
@@ -5,7 +5,7 @@ $targetID = $_GET['targetID'];
 Enter your bounty amount:
 
 NOTE: need to put error checking/validation
-<form action='backend/addtobountylist.php' method='GET'/>
+<form action='backend/addtobountylist.php' method='GET'>
 <input type='text' name='bountyAmount'/>
 <input type='hidden' name='targetID' value='<?php echo $targetID;?>'/>
 <input type='submit' value='Place Bounty'/>
