b81a0c731af239bc508cdc8fcfc64468f02d27de
Conrad Chan <conradchan@Conrad-Chans-MacBook-Pro.local>
2011-08-17 00:52:46 -0700

    added reminder for validation

--- addplayertobounty.php
+++ addplayertobounty.php
@@ -4,7 +4,8 @@ $targetID = $_GET['targetID'];
 ?>
 Enter your bounty amount:
 
-NOTE: need to put error checking/validation
+NOTE: need to put error checking/validation to see if its a positive int <br>
+and backend needs to check if user has enough money
 <form action='<?php $_SERVER['DOCUMENT_ROOT'] ?>/backend/addtobountylist.php' method='GET'>
 <input type='text' name='bountyAmount'/>
 <input type='hidden' name='targetID' value='<?php echo $targetID;?>'/>
