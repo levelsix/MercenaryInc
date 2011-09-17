5ba2e73d23199b58d8913237ef799d3af0872570
Conrad Chan <conradchan@Conrad-Chans-MacBook-Pro.local>
2011-09-17 10:52:46 -0700

    refactored shopaction.php

--- backend/shopaction.php
+++ backend/shopaction.php
@@ -2,6 +2,7 @@
 include_once($_SERVER['DOCUMENT_ROOT'] . "/classes/ConnectionFactory.php");
 include_once($_SERVER['DOCUMENT_ROOT'] . "/properties/serverproperties.php");
 include_once($_SERVER['DOCUMENT_ROOT'] . "/properties/dbcolumnnames.php");
+include_once($_SERVER['DOCUMENT_ROOT'] . "/classes/User.php");
 
 $SELL_RATIO = .6;
 
@@ -10,52 +11,14 @@ $itemID = $_POST['itemID'];
 $storePrice = $_POST['storePrice'];
 
 session_start();
-
-$db = ConnectionFactory::getFactory()->getConnection();
-
-$currentStmt = $db->prepare("SELECT * FROM users_items WHERE user_id = ? AND item_id = ?");
-$currentStmt->execute(array($_SESSION['userID'], $itemID));
-
-$numCurrent = $currentStmt->rowCount();
-
-//$currentResult will be null if the user doesn't have that item
-$currentResult = $currentStmt->fetch(PDO::FETCH_ASSOC);
+$user = User::getUser($_SESSION['userID']);
 
 if ($actionToDo == 'buy') {
-	if ($numCurrent == 0) {
-		$stmt = $db->prepare("INSERT INTO users_items (user_id, item_id, quantity) VALUES (?, ?, 1)");
-		$params = array($_SESSION['userID'], $itemID);
-	} else {
-		$stmt = $db->prepare("UPDATE users_items SET quantity = quantity + 1 WHERE user_id = ? AND item_id = ?");
-		$params = array($_SESSION['userID'], $itemID);
-	}
+	$user->incrementUserItem($itemID, 1);
+	$user->updateUserCash($storePrice*-1);
 } else if ($actionToDo = 'sell') {	
-	if ($numCurrent > 0) {	//should always be, but just in case
-		if ($currentResult['quantity'] == 1) {
-			$stmt = $db->prepare("DELETE FROM users_items WHERE user_id = ? AND item_id = ?");
-			$params = array($_SESSION['userID'], $itemID);
-		} else {
-			$stmt = $db->prepare("UPDATE users_items SET quantity = quantity - 1 WHERE user_id = ? AND item_id = ?");
-			$params = array($_SESSION['userID'], $itemID);
-		}
-	}	
-}
-if (!($stmt->execute($params))) {
-	header("Location: $serverRoot/errorpage.html");
-	exit;
-}
-
-// Update cash
-if ($actionToDo == 'buy') {
-	$stmt = $db->prepare("UPDATE users SET cash = cash - ? WHERE id = ?");
-	$params = array($storePrice, $_SESSION['userID']);
-} else if ($actionToDo = 'sell') {	
-	$stmt = $db->prepare("UPDATE users SET cash = cash + ? WHERE id = ?");
-	$params = array($storePrice * $SELL_RATIO, $_SESSION['userID']);
-}
-if (!($stmt->execute($params))) {
-	header("Location: $serverRoot/errorpage.html");
-	exit;
+	$user->decrementUserItem($itemID, 1);
+	$user->updateUserCash($storePrice);
 }
 
 header("Location: $serverRoot/shoplist.php");
